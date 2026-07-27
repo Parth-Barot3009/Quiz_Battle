1456c2a926e81965310d8e9499e657fefc2b65f3
vishwagondaliyak-cmd <vishwagondaliya.k@gmail.com>
2026-07-27 23:05:55 +0530

    update controller on create battle

--- lib/organizer/create_battle.dart
+++ lib/organizer/create_battle.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-// import 'package:flutter_time_picker_spinner/flutter_time_picker_spinner.dart';
+import 'dart:math';
 import 'package:file_picker/file_picker.dart';
+import 'package:quiz_battle/organizer/Battle_Room_Org.dart';
 
 class create_battle extends StatefulWidget {
   const create_battle({super.key});
@@ -15,6 +17,9 @@ class _create_battleState extends State<create_battle> {
   TimeOfDay? endTime;
   String? selectedFileName;
   DateTime? selectedDate;
+  int totalQuestions = 20;
+  final TextEditingController roomname = TextEditingController();
+  final TextEditingController roomCodeController = TextEditingController();
 
   //time pick class
 
@@ -76,7 +81,26 @@ class _create_battleState extends State<create_battle> {
     }
   }
 
+  //roomcode
+
+  void generateRoomCode() {
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    final random = Random();
+
+    String code = "";
+
+    for (int i = 0; i < 6; i++) {
+      code += characters[random.nextInt(characters.length)];
+    }
+
+    roomCodeController.text = code;
+  }
+
   @override
+  void initState() {
+    super.initState();
+    generateRoomCode();
+  }
   Widget build(BuildContext context) {
 
     // final screenWidth = MediaQuery.of(context).size.width;
@@ -99,24 +123,8 @@ class _create_battleState extends State<create_battle> {
             ),
           ),
         ),
-        title: Text("Create Battle Room",style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white,fontFamily: "BaiJamjuree",),),
-        leading: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                shape: BoxShape.circle,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black12,
-                    blurRadius: 8,
-                    offset: Offset(0, 3),
-                  ),
-                ],
-              ),
-              child: IconButton(onPressed: (){}, icon: Icon(Icons.arrow_back),iconSize: 25,color: Color(0xFF4A7CFF),)
-          ),
-        ),
+        title: Text("Create Battle Room",style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white,),),
+        automaticallyImplyLeading: false,
       ),
 
       body: SingleChildScrollView(
@@ -148,16 +156,15 @@ class _create_battleState extends State<create_battle> {
                           color: Colors.black,
                           fontWeight: FontWeight.bold,
                           fontSize: 20,
-                          fontFamily: "BaiJamjuree",
                         ),
                         ),
 
                         SizedBox(height: 15),
 
                         TextFormField(
+                          controller: roomname,
                           decoration: InputDecoration(
                             hintText: "Enter Room Name",
-
                             prefixIcon: Padding(
                               padding: const EdgeInsets.all(10),
                               child: Container(
@@ -224,8 +231,11 @@ class _create_battleState extends State<create_battle> {
                         SizedBox(height: 15),
 
                         TextFormField(
+                          controller: roomCodeController,
+                          readOnly: true,
+
                           decoration: InputDecoration(
-                            hintText: "Enter Room Code",
+                            hintText: "Room Code",
 
                             prefixIcon: Padding(
                               padding: const EdgeInsets.all(10),
@@ -238,7 +248,7 @@ class _create_battleState extends State<create_battle> {
                                   borderRadius: BorderRadius.circular(10),
                                 ),
                                 child: const Icon(
-                                  Icons.numbers_rounded,
+                                  Icons.vpn_key_rounded,
                                   color: Color(0xFF4A6CF7),
                                   size: 22,
                                 ),
@@ -265,19 +275,8 @@ class _create_battleState extends State<create_battle> {
 
                             enabledBorder: OutlineInputBorder(
                               borderRadius: BorderRadius.circular(18),
-                              borderSide: BorderSide(
+                              borderSide: const BorderSide(
                                 color: Color(0xFFE0E0E0),
-                              ),
-                            ),
-
-                            suffixIcon: IconButton(
-                              onPressed: () {
-                                // Your refresh code here
-                              },
-                              icon: const Icon(
-                                Icons.refresh,
-                                color: Color(0xFF4A6CF7),
-                                size: 26,
                               ),
                             ),
 
@@ -285,6 +284,19 @@ class _create_battleState extends State<create_battle> {
                               borderRadius: BorderRadius.circular(18),
                               borderSide: const BorderSide(
                                 color: Color(0xFF4A6CF7),
+                              ),
+                            ),
+
+                            suffixIcon: IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  generateRoomCode();
+                                });
+                              },
+                              icon: const Icon(
+                                Icons.refresh_rounded,
+                                color: Color(0xFF4A6CF7),
+                                size: 28,
                               ),
                             ),
                           ),
@@ -374,6 +386,126 @@ class _create_battleState extends State<create_battle> {
                           ],
                         ),
 
+                        // Total Questions
+
+                        const SizedBox(height: 20),
+
+                        const Text(
+                          "Questions for Battle",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+
+                          ),
+                        ),
+
+                        const SizedBox(height: 15),
+
+                        Container(
+                          height: 65,
+                          padding: const EdgeInsets.symmetric(horizontal: 12),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(18),
+                            border: Border.all(
+                              color: const Color(0xFFE0E0E0),
+                            ),
+                          ),
+                          child: Row(
+                            children: [
+
+                              // Left Icon
+                              Container(
+                                width: 44,
+                                height: 44,
+                                decoration: BoxDecoration(
+                                  color: const Color(0xFFE9ECFF),
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                child: const Icon(
+                                  Icons.quiz_rounded,
+                                  color: Color(0xFF4A6CF7),
+                                ),
+                              ),
+
+                              const SizedBox(width: 15),
+
+                              const Expanded(
+                                child: Text(
+                                  "Questions",
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    color: Colors.grey,
+
+                                  ),
+                                ),
+                              ),
+
+                              // Minus Button
+                              InkWell(
+                                onTap: () {
+                                  if (totalQuestions > 1) {
+                                    setState(() {
+                                      totalQuestions--;
+                                    });
+                                  }
+                                },
+                                borderRadius: BorderRadius.circular(8),
+                                child: Container(
+                                  width: 36,
+                                  height: 36,
+                                  decoration: BoxDecoration(
+                                    color: const Color(0xFFE9ECFF),
+                                    borderRadius: BorderRadius.circular(8),
+                                  ),
+                                  child: const Icon(
+                                    Icons.remove,
+                                    color: Color(0xFF4A6CF7),
+                                  ),
+                                ),
+                              ),
+
+                              const SizedBox(width: 12),
+
+                              Text(
+                                "$totalQuestions",
+                                style: const TextStyle(
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xFF4A6CF7),
+                                ),
+                              ),
+
+                              const SizedBox(width: 12),
+
+                              // Plus Button
+                              InkWell(
+                                onTap: () {
+                                  if (totalQuestions < 50) {
+                                    setState(() {
+                                      totalQuestions++;
+                                    });
+                                  }
+                                },
+                                borderRadius: BorderRadius.circular(8),
+                                child: Container(
+                                  width: 36,
+                                  height: 36,
+                                  decoration: BoxDecoration(
+                                    color: const Color(0xFFE9ECFF),
+                                    borderRadius: BorderRadius.circular(8),
+                                  ),
+                                  child: const Icon(
+                                    Icons.add,
+                                    color: Color(0xFF4A6CF7),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+
                         SizedBox(height: 20),
 
                         //start time
@@ -399,7 +531,6 @@ class _create_battleState extends State<create_battle> {
                           ),
                           child: Row(
                             children: [
-
                               // Left Icon
                               Container(
                                 width: 44,
@@ -535,6 +666,8 @@ class _create_battleState extends State<create_battle> {
                           ),
                         ),
 
+                        const SizedBox(height: 15),
+
                         //date
 
                         Column(
@@ -614,6 +747,42 @@ class _create_battleState extends State<create_battle> {
                                     ),
                                   ),
                                 ],
+                              ),
+                            ),
+                            const SizedBox(height: 30),
+
+                            SizedBox(
+                              width: double.infinity,
+                              height: 60,
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  Navigator.push(context, MaterialPageRoute(builder: (context)=>Org_BattleRoom()));
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: const Color(0xFF4A6CF7),
+                                  foregroundColor: Colors.white,
+                                  elevation: 3,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18),
+                                  ),
+                                ),
+                                child: const Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Icon(
+                                      Icons.sports_esports_rounded,
+                                      size: 24,
+                                    ),
+                                    SizedBox(width: 10),
+                                    Text(
+                                      "Create Battle",
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ],
+                                ),
                               ),
                             ),
                           ],
