5ab6ae4abb59086b9b2b3d3bd416b8566e12d5c7
Parth Barot <parthbarot925@gmail.com>
2026-08-01 23:00:53 +0530

    Add Quiz Logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:quiz_battle/Demo.dart';
-import 'package:quiz_battle/admin/Admin_Deshboard.dart';
-import 'package:quiz_battle/admin/Navigation_Admin.dart';
-import 'package:quiz_battle/admin/Organizer(List_Screen).dart';
-import 'package:quiz_battle/admin/Student_ListScreen.dart';
-import 'package:quiz_battle/admin/addorganiser.dart';
 import 'package:quiz_battle/auth/Splash_Screen.dart';
-import 'package:quiz_battle/auth/login_admin_organiser.dart';
-import 'package:quiz_battle/organizer/Battle_Room_Org.dart';
-import 'package:quiz_battle/organizer/Organizer_Battle_History.dart';
-import 'package:quiz_battle/organizer/ProfileInfo_Organizer.dart';
-import 'package:quiz_battle/organizer/create_battle.dart';
-import 'package:quiz_battle/organizer/organizer_dashboard.dart';
-import 'package:quiz_battle/organizer/organizer_navigationbar.dart';
-import 'package:quiz_battle/player/join_battle.dart';
-import 'package:quiz_battle/player/player_navigationbar.dart';
-import 'package:quiz_battle/player/user_dashboard.dart';
-import 'package:quiz_battle/player/user_profile.dart';
-import 'package:quiz_battle/player/waiting_room.dart';
 import 'firebase_options.dart';
 
 void main() async {
